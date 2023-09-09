0d785c3c7675e80a0ed2fd54f2a586877680342f
ray-sarthak <117006899+ray-sarthak@users.noreply.github.com>
2023-09-09 16:34:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 //your code here
+const imgs=document.getElementsByClassName("image");
+let count=1;
+let id='';
+
+for(let i=0;i<imgs.length;i++){
+	
+	imgs[i].setAttribute ("id",`div${i+1}`);
+	let img=imgs[i];
+	
+	img.addEventListener('dragstart', (e)=>{
+		id=img.id;
+		setTimeout(()=>{
+			img.id=x;
+		})   
+		
+		
+		
+	})
+	img.addEventListener('dragover', (e)=>{
+		e.preventDefault()
+		
+		
+	})
+	}
+for(let i=0;i<imgs.length;i++){
+	
+	
+	let img=imgs[i];
+
+	img.addEventListener('drop',(e)=>{
+		e.preventDefault();
+		x=e.target.id;
+	
+		
+		e.target.id=id; 
+		id=x;
+		
+		console.log(e.target);   
+	})
+	}
+

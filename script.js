e0bdb9baa152f5948ec6e46425cec08a99fbf817
ray-sarthak <117006899+ray-sarthak@users.noreply.github.com>
2023-09-10 13:23:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,47 @@
 //your code here
 const imgs=document.getElementsByClassName("image");
-let count=1;
-let id='';
+let dragindex=0;
+let dropindex=0;
+let clone="";
 
 for(let i=0;i<imgs.length;i++){
 	
 	imgs[i].setAttribute ("id",`div${i+1}`);
 	let img=imgs[i];
+	imgs[i].setAttribute("ondragstart","drag(event)");
+	imgs[i].setAttribute("ondrop","drop(event)");
+	imgs[i].setAttribute("ondragover","allowDrop(event)");
 	
-	img.addEventListener('dragstart', (e)=>{
-		id=img.id;
-		setTimeout(()=>{
-			img.id=x;
-		})   
-		
-		
-		
-	})
-	img.addEventListener('dragover', (e)=>{
-		e.preventDefault()
-		
-		
-	})
-	}
-for(let i=0;i<imgs.length;i++){
-	
-	
-	let img=imgs[i];
+}
+/////////
+function drag(e)
+{
+e.dataTransfer.setData("text",e.target.id);
+}
+function drop(e)
+{
+e.preventDefault();
+clone=e.target.cloneNode(true);
+let data=e.dataTransfer.getData("text"); 
+if(clone.id !== data) {
+let nodelist=document.getElementById("parent").childNodes;
+for(let i=0;i<nodelist.length;i++)
+{
+if(nodelist[i].id==data)
+{
+dragindex=i;
+}
 
-	img.addEventListener('drop',(e)=>{
-		e.preventDefault();
-		x=e.target.id;
-	
-		
-		e.target.id=id; 
-		id=x;
-		
-		console.log(e.target);   
-	})
-	}
+}
 
+document.getElementById("parent").replaceChild(document.getElementById(data),e.target);
+
+document.getElementById("parent").insertBefore(clone,document.getElementById("parent").childNodes[dragindex]);
+  }
+
+}
+
+function allowDrop(e)
+{
+    e.preventDefault();
+}
